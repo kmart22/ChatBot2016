9cd039b9d80e5eae7fae6181e4a810c3e0a7f85b
kmart22 <kmar7070@740s-pl-111.local>
2016-10-14 08:54:36 -0600

    added start to project

--- src/chat/controller/ChatbotRunner.java
+++ src/chat/controller/ChatbotRunner.java
@@ -2,5 +2,10 @@ package chat.controller;
 
 public class ChatbotRunner
 {
-
+	
+	public static void main (String [] args)
+	{
+		ChatbotController chatAppController = new ChatbotController();
+		chatAppController.start();
+	}
 }
