3d195535071041426b78eeb590155a6485b29faa
kmart22 <kmar7070@740s-pl-111.local>
2016-10-26 08:03:18 -0600

    added response to what the user wants to talk about

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -16,11 +16,32 @@ public class ChatController
 	
 	public void start()
 	{
-		String response = "words";
+		String response = display.collectResponse("What do you want to talk about?");
 		
 		while(stupidBot.lengthChecker(response))
 		{
-			response = display.collectResponse("do you want to know how to get ladies?");
+			display.displayMessage(useChatbotCheckers(response));
+			response = display.collectResponse("oh, you want to talk about " + response + "? tell me more...");
 		}
+	}
+	
+	private String useChatbotCheckers(String input)
+	{
+		String checkedInput = "";
+		
+		if(stupidBot.memeChecker(input))
+		{
+			checkedInput += "\nYou like memes!\n";
+		}
+		if(stupidBot.contentChecker(input))
+		{
+			checkedInput += "\nYou know my secret topic!n";
+		}
+		if(checkedInput.length() == 0)
+		{
+			checkedInput = "I have no idea what you mean about " + input;
+		}
+		
+		return checkedInput;
 	}
 }
